e14643b0aff20502bdbda32e5feeccf792d6e597
hqbly <yygoodcccchu@gmail.com>
2026-09-20 22:25:56 +0800

    refactor: 项目构建

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-// Movie Design Web - 入口脚本
-(function () {
-  'use strict';
-  document.addEventListener('DOMContentLoaded', function () {
-    console.log('Movie Design Web initialized.');
-  });
-})();
